f7e2d170cf0fcc9ae37b8e85df3c1562130b657f
Sara A <calisaraalhindi@gmail.com>
2020-10-05 17:33:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,11 +43,16 @@ class Student
     end
   end
   
+<<<<<<< HEAD
   def self.create(name, grade)
+=======
+  def self.create(name:, grade:)
+>>>>>>> a4770f61d227168521d993f3f734c96da4579693
     student = Student.new(name, grade)
     student.save
     student
   end
+<<<<<<< HEAD
   
   def self.new_from_db(row)
     grade = row[0]
@@ -67,6 +72,18 @@ class Student
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
+=======
+ 
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[0], result[1], result[2])
+  end
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE name = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.name)
+>>>>>>> a4770f61d227168521d993f3f734c96da4579693
   end
 end
 
