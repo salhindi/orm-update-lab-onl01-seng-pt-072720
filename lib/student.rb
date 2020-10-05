45827422cde8c9af4bc251c6dd0b5ee5b53c635c
Sara A <calisaraalhindi@gmail.com>
2020-10-05 17:36:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -73,7 +73,7 @@ class Student
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
-    Student.new(result[0], result[1], result[2])
+    
   end
 
   def update
